79445ba6c5f57940d6c38d330725a4c58e2305c5
Denis B. <Deniscapital@mail.ru>
2024-04-07 19:48:23 +0300

    Создали Семи1 и написали первую задачу

---
+++ Leson2/Semi1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа сравнивает квадрат числа Console.ReadLine(); - команда ожидания ввода пользователя
+// "=" это присвоение "==" это сравнение
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 == num2 * num2)
+{
+  Console.WriteLine("является квадратом");
+}
+
+else
+{
+  Console.WriteLine("НЕ является квадратом числа");
+}
+
+
+
