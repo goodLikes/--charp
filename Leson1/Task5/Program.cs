d1f3f3d463f700acce360044deaa26c64bb625d6
Denis B. <Deniscapital@mail.ru>
2024-04-07 18:20:39 +0300

    Добавили Таск5 сравнение гирь

---
+++ Leson1/Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿// Моя 5 задача гирьки найти самую большую
+
+int a = 9;
+int b = 3;
+int c = 10;
+int d = 2;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+  max = b;
+}
+if (c > max)
+{
+  max = c;
+}
+if (d > max)
+{
+  max = d;
+}
+if (e > max)
+{
+  max = e;
+}
+
+Console.WriteLine(max);
