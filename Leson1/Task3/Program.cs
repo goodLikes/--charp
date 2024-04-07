e3e4337d016246e83b9f0d4e3b6d2aaa2d7c1977
Denis B. <Deniscapital@mail.ru>
2024-04-07 18:09:33 +0300

    git commit -m "созадли Таск3"

---
+++ Leson1/Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// МОЯ ТРЕТЬЯ ПРОГРАММА
+
+Double a = 5.0;
+Double b = 2.0;
+Double c = 6.0;
+Double d = 6.0;
+
+Double pro = a * b;
+Double sum = c + d;
+Double Total = pro / sum;
+
+Console.WriteLine(Total);
+
