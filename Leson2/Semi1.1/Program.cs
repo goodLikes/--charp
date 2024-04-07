90e1a087747dd790d4f16b4f180b9042debf3a78
Denis B. <Deniscapital@mail.ru>
2024-04-07 20:14:03 +0300

    создали Семи1.1 и написали код

---
+++ Leson2/Semi1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа на вход принимает целое число N, а на выходе показывает все целые числа в промежутке от -N  до N
+// Primer :  4 => -4 , -3 , -2 , -1 , 0 , 1 , 2, 3, 4
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int NegativeNum = -num1;
+
+while (NegativeNum <= num1)
+{
+  Console.Write(NegativeNum + " ");
+  // или Console.WriteLine(NegativeNum);
+  NegativeNum++;
+}
