5db4384dac3cf009c9eb8883e8fb63c9b6e82a41
Denis B. <Deniscapital@mail.ru>
2024-04-07 17:33:50 +0300

    создали Таск1 и написали код

---
+++ Leson1/Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// МОЯ ПЕРВАЯ ЗАДАЧА
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
