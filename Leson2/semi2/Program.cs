e9d26a90bce29d48edf1d67acfb3f03d97bc01da
Denis B. <Deniscapital@mail.ru>
2024-04-11 21:46:09 +0300

    create semi2 masive

---
+++ Leson2/semi2/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[6];
+
+/* int i = 0;
+while (i < 6)
+{
+  i++;
+} */
+for (int i = 0; i < 3; i++)
+{
+  array[i] = new Random().Next(1, 10); // можно указать nextdouble() и будет не целые числа
+  Console.WriteLine(array[i] + " ");
+}
+
+
