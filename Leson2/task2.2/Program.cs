a1b7ff72e4d3986df977558e49d483801c3ca1f9
Denis B. <Deniscapital@mail.ru>
2024-04-11 16:30:17 +0300

    Создали Таск2.2 масивы перечесление

---
+++ Leson2/task2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+//while (i < n)  можно так пользоваться для поиска ,а можно вот так
+while (i < arr.Length)
+{
+  arr[i] = i + 1;
+  //Console.Write(arr[i]);
+  //Console.Write(' ');
+  Console.Write($"{arr[i]} "); // или такая команда для вывода в одну строку масивов
+  i = i + 1;
+}
