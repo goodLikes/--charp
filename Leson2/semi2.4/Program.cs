af9864bf4afafc345ab0cc2a363df88bb6ab3bc3
Denis B. <Deniscapital@mail.ru>
2024-04-11 23:16:20 +0300

    create semi2.4

---
+++ Leson2/semi2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Дано натуральное трёхзначное число. Создайте массив, состоящий из 
+цифр этого числа. Младший разряд числа должен располагаться на 0-
+м индексе массива, старший – на 2-м. 
+Пример
+456 => [6 5 4]
+781 => [1 8 7] */
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[3];
+
+for (int i = 0; i < array.Length; i++, num /= 10)
+{
+  array[i] = num % 10;
+  Console.Write(array[i] + "\t");
+  // num = num/10;
+}
