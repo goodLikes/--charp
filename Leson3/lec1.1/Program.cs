ee096afdd981852adc4de85bd6a5c7b5427e754b
Denis B. <Deniscapital@mail.ru>
2024-04-17 16:09:58 +0300

    cоздали лесон3 с задачами

---
+++ Leson3/lec1.1/Program.cs
@@ -0,0 +1,12 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+  int numenator = a * b;
+  double dominator = c + d;
+  double result = numenator / dominator;
+  return result;
+}
+
+CalculateFormula(1, 2, 3, 4);
+
+double answer = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(answer);
